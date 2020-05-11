38666e7f11cd717cb503a4a2e689aa87a45974de
rulitka <59177208+rulitka@users.noreply.github.com>
2020-05-11 19:43:50 +0300

    Update auto_post_api.py

--- auto_post_api.py
+++ auto_post_api.py
@@ -1,4 +1,3 @@
-  
 import glob
 import os
 import sys
@@ -7,58 +6,67 @@ from io import open
 from instabot import Bot  # noqa: E402
 from dotenv import load_dotenv
 
-sys.path.append(os.path.join(sys.path[0], "../../"))
-load_dotenv()
-username = os.getenv('INSTA_LOGIN')
-password = os.getenv('INSTA_PASSWORD')
-posted_pic_list = []
-try:
-    with open("C/images/upload_images.txt", "r", encoding="utf8") as f:
-        posted_pic_list = f.read().splitlines()
-except Exception:
+def post_file():
+    sys.path.append(os.path.join(sys.path[0], "../../"))
+    load_dotenv()
+    username = os.getenv('INSTA_LOGIN')
+    password = os.getenv('INSTA_PASSWORD')
     posted_pic_list = []
-
-timeout = 24 * 60 * 60  # pics will be posted every 24 hours
-
-bot = Bot()
-bot.login(username = username,  
-          password = password)
-
-while True:
-    folder_path = "C:/images/"
-    pics = glob.glob(folder_path + "/*_crop.jpg")
-    pics = sorted(pics)
     try:
-        for pic in pics:
-            if pic in posted_pic_list:
-                continue
+        with open("./images/upload_images.txt", "r", encoding="utf8") as f:
+            posted_pic_list = f.read().splitlines()
+    except Exception:
+        posted_pic_list = []
 
-            pic_name = pic[:-4].split("-")
-            pic_name = "-".join(pic_name[1:])
+    timeout = 24 * 60 * 60  # pics will be posted every 24 hours
 
-            print("upload: " + pic_name)
+    bot = Bot()
+    bot.login(username = username,  
+              password = password)
 
-            description_file = folder_path + "/" + pic_name + ".txt"
+    folder_path = "./images/"
+    pics = glob.glob(folder_path + "/*_crop.jpg")
+    pics = sorted(pics) 
+    for pic in pics:
+        try:
+            for pic in pics:
+                if pic in posted_pic_list:
+                    continue
 
-            if os.path.isfile(description_file):
-                with open(description_file, "r") as file:
-                    caption = file.read()
-            else:
-                caption = pic_name.replace("-", " ")
+                pic_name = pic[:-4].split("-")
+                pic_name = "-".join(pic_name[1:])
 
-            bot.upload_photo(pic, caption=caption)
-            if bot.api.last_response.status_code != 200:
-                print(bot.api.last_response)
-                # snd msg
-                break
+                print("upload: " + pic_name)
 
-            if pic not in posted_pic_list:
-                posted_pic_list.append(pic)
-                with open("C/images/upload_images.txt", "a", encoding="utf8") as f:
-                    f.write(pic + "\n")
+                description_file = os.path.join('folder_path', 'pic_name + ".txt"')
 
-            time.sleep(timeout)
+                if os.path.isfile(description_file):
+                    with open(description_file, "r") as file:
+                        caption = file.read()
+                else:
+                    caption = pic_name.replace("-", " ")
 
-    except Exception as e:
-        print(str(e))
-    time.sleep(60)
+                bot.upload_photo(pic, caption=caption)
+                if bot.api.last_response.ok:
+                    pass
+                else:
+                    break
+
+                if pic not in posted_pic_list:
+                    posted_pic_list.append(pic)
+                    with open("./images/upload_images.txt", "a", encoding="utf8") as f:
+                        f.write(pic + "\n")
+
+                time.sleep(timeout)
+
+        except (FileNotFoundError, IOError):
+            print("Wrong file or file path")
+        time.sleep(60)
+
+
+def main():
+    post_file()
+
+
+if __name__ == '__main__':
+    main()
