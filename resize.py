5a80a8468d986848927b9f2ce785e48f6d24acb9
rulitka <59177208+rulitka@users.noreply.github.com>
2020-05-11 19:45:55 +0300

    Add files via upload

---
+++ resize.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from PIL import Image
+
+
+def resize_aspect_fit():
+    path = './images/'
+    dir = os.listdir(path)
+    final_size = 1080;
+    for file_name in dir:
+        if os.path.isfile(path+file_name):
+            image = Image.open(path+file_name)
+            file, file_extension = os.path.splitext(path+file_name)
+            size = image.size
+            ratio = float(final_size) / max(size)
+            new_image_size = tuple([int(x*ratio) for x in size])
+            image = image.resize(new_image_size, Image.ANTIALIAS)
+            new_image = Image.new("RGB", (final_size, final_size))
+            new_image.paste(image, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+            file_new_name = f'{file}_resize{file_extension}'
+            new_image.paste(image, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
+        return new_image
