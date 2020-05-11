73cdfa74eb9ed0fafd0c3a38fef9600ea4540391
rulitka <59177208+rulitka@users.noreply.github.com>
2020-05-11 19:44:30 +0300

    Update fetch_hubble.py

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -2,9 +2,10 @@ import requests
 from pathlib import Path
 import os
 from PIL import Image
+import resize1
 
 
-def return_of_collection(collection_name):
+def get_collection(collection_name):
     url = f'http://hubblesite.org/api/v3/images/{collection_name}'
     launches_response = requests.get(url)
     launches_response.raise_for_status()
@@ -15,47 +16,32 @@ def return_of_collection(collection_name):
     return image_id
 
 
-def return_of_images(image_id):
+def get_image(image_id):
     url = f'http://hubblesite.org/api/v3/image/{image_id}'
     launches_response = requests.get(url)
     launches_response.raise_for_status()
     hubble_image = launches_response.json()['image_files'][-1]['file_url']
     full_file_url = f'https:{hubble_image}'
-    file_extension = os.path.splitext(full_file_url)
-    image_name = (f'hubble_{image_id}.' + file_extension)
-    save_dir = Path('/images/')
+    file_extension = os.path.splitext(full_file_url)[-1]
+    image_name = (f'hubble_{image_id}{file_extension}')
+    save_dir = Path('./images/')
     save_dir.mkdir(parents=True, exist_ok=True)
     file_path = save_dir / image_name
     response = requests.get(full_file_url, verify=False)
     response.raise_for_status()
     with open(file_path, 'wb') as file:
         file.write(response.content)
-    new_image = resize_aspect_fit()
+    new_image = resize1.resize_aspect_fit()
     new_image.save(f'{save_dir}/hubble_{image_id}_crop.jpg', 'JPEG')
     file_path.unlink()
 
 
-def resize_aspect_fit():
-    path = '/images/'
-    dir = os.listdir(path)
-    final_size = 1080;
-    for photo in dir:
-        im = Image.open(path+photo)
-        f, e = os.path.splitext(path+photo)
-        size = im.size
-        ratio = float(final_size) / max(size)
-        new_image_size = tuple([int(x*ratio) for x in size])
-        im = im.resize(new_image_size, Image.ANTIALIAS)
-        new_im = Image.new("RGB", (final_size, final_size))
-        new_im.paste(im, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
-    return new_im
-
-
 def main():
     collection_name = input ("Введите название коллекции:")
-    image_id = return_of_collection(collection_name)
+    image_id = get_collection(collection_name)
     for image in image_id:
-        return_of_images(image)
+        get_image(image)
+
 
 
 if __name__ == '__main__':
