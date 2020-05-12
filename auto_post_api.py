7f74f1f10a962ee135a4fbc82e6946fb59c67da4
rulitka <59177208+rulitka@users.noreply.github.com>
2020-05-12 21:04:08 +0300

    Update auto_post_api.py

--- auto_post_api.py
+++ auto_post_api.py
@@ -6,20 +6,15 @@ from io import open
 from instabot import Bot  # noqa: E402
 from dotenv import load_dotenv
 
-def post_file():
+def post_file(username, password):
     sys.path.append(os.path.join(sys.path[0], "../../"))
     load_dotenv()
-    username = os.getenv('INSTA_LOGIN')
-    password = os.getenv('INSTA_PASSWORD')
     posted_pic_list = []
     try:
         with open("./images/upload_images.txt", "r", encoding="utf8") as f:
             posted_pic_list = f.read().splitlines()
     except (FileNotFoundError, IOError):
         posted_pic_list = []
-
-    timeout = 24 * 60 * 60  # pics will be posted every 24 hours
-
     bot = Bot()
     bot.login(username = username,  
               password = password)
@@ -29,22 +24,8 @@ def post_file():
     pics = sorted(pics) 
     for pic in pics:
         try:
-            for pic in pics:
                 if pic in posted_pic_list:
                     continue
-
-                pic_name = pic[:-4].split("-")
-                pic_name = "-".join(pic_name[1:])
-
-                print("upload: " + pic_name)
-
-                description_file = os.path.join('folder_path', 'pic_name + ".txt"')
-
-                if os.path.isfile(description_file):
-                    with open(description_file, "r") as file:
-                        caption = file.read()
-                else:
-                    caption = pic_name.replace("-", " ")
 
                 bot.upload_photo(pic, caption=caption)
                 if bot.api.last_response.ok:
@@ -65,7 +46,9 @@ def post_file():
 
 
 def main():
-    post_file()
+    username = os.getenv('INSTA_LOGIN')
+    password = os.getenv('INSTA_PASSWORD')
+    post_file(username, password)
 
 
 if __name__ == '__main__':
