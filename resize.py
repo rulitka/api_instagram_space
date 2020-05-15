ec5485b5b2743f2f97c39aed2ab7ea11885f905e
rulitka <59177208+rulitka@users.noreply.github.com>
2020-05-15 17:08:03 +0300

    Update resize.py

--- resize.py
+++ resize.py
@@ -14,10 +14,10 @@ def resize_aspect_fit():
             file, file_extension = os.path.splitext(path+file_name)
             size = image.size
             ratio = float(final_size) / max(size)
-            new_image_size = tuple([int(x*ratio) for x in size])
+            width, height = tuple([int(x*ratio) for x in size])
+            new_image_size = [width, height]
             image = image.resize(new_image_size, Image.ANTIALIAS)
             new_image = Image.new("RGB", (final_size, final_size))
-            new_image.paste(image, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
-            file_new_name = f'{file}_resize{file_extension}'
-            new_image.paste(image, ((final_size-new_image_size[0])//2, (final_size-new_image_size[1])//2))
-        return new_image
+            new_image.paste(image, ((final_size-width)//2, (final_size-height)//2))
+            new_image.save(f'{file_path}_crop.jpg', 'JPEG')
+    return new_image
