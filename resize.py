9b318629a007371df6851f934ea82e2ae42a21f4
rulitka <59177208+rulitka@users.noreply.github.com>
2020-05-16 13:59:52 +0300

    Update resize.py

--- resize.py
+++ resize.py
@@ -3,21 +3,22 @@ import os
 from PIL import Image
 
 
-def resize_aspect_fit():
-    path = './images/'
-    folder = os.listdir(path)
+def resize_aspect_fit(file_path):
     final_size = 1080;
-    for file_name in folder:
-        file_path = os.path.join(path, file_name)
-        if os.path.isfile(file_path):
-            image = Image.open(file_path)
-            file, file_extension = os.path.splitext(path+file_name)
-            size = image.size
-            ratio = float(final_size) / max(size)
-            width, height = tuple([int(x*ratio) for x in size])
-            new_image_size = [width, height]
-            image = image.resize(new_image_size, Image.ANTIALIAS)
-            new_image = Image.new("RGB", (final_size, final_size))
-            new_image.paste(image, ((final_size-width)//2, (final_size-height)//2))
-            new_image.save(f'{file_path}_crop.jpg', 'JPEG')
+    image = Image.open(file_path)
+    size = image.size
+    ratio = float(final_size) / max(size)
+    width, height = tuple([int(x*ratio) for x in size])
+    new_image_size = [width, height]
+    image = image.resize(new_image_size, Image.ANTIALIAS)
+    new_image = Image.new("RGB", (final_size, final_size))
+    new_image.paste(image, ((final_size-width)//2, (final_size-height)//2))
     return new_image
+
+
+def main():
+    resize_aspect_fit(image)
+
+
+if __name__ == '__main__':
+    main()
